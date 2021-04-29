59fd009f125366bb61620bc81cd74b2d82cc1f70
a.anikin <79037128326@ya.ru>
2021-04-29 12:00:44 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,364 @@
+const stands = {
+        'left-top': {
+            'top': 410,
+            'left':80,
+            'data': [
+                {
+                    id: 20,
+                    title: "Юнармия",
+                    description: 'Тест'
+                },
+                {
+                    id: 21,
+                    title: "Армия культуры",
+                    description: 'Тест'
+                },
+                {
+                    id: 22,
+                    title: "Киберпатриот",
+                    description: 'Тест'
+                },
+                {
+                    id: 23,
+                    title: "Меридиан",
+                    description: 'Тест'
+                },
+                {
+                    id: 24,
+                    title: "Безопасная среда",
+                    image: "17-sreda.png",
+                    description: 'Соревнования экипажей радиационной, химической и биологической разведки'
+                },
+                {
+                    id: 25,
+                    title: "Уверенный прием",
+                    image: "12-priem.png",
+                    description: 'Соревнования подразделений связи'
+                },
+                {
+                    id: 26,
+                    title: "Мастера автобр. техн.",
+                    image: "18-atb.png",
+                    description: 'Совернования специалистов автотехнического и танкотехнического обеспечения'
+                },
+                {
+                    id: 27,
+                    title: "Военное ралли",
+                    image: "24-ralli.png",
+                    description: 'Соревнования экипажей на военной на военной автомобильной техники'
+                },
+                {
+                    id: 28,
+                    title: "Страж порядка",
+                    image: "23-strazh.png",
+                    description: 'Соревнования военнослужащих военной полиции'
+                },
+                {
+                    id: 29,
+                    title: "Дорожный патруль",
+                    image: "27-patrul.png",
+                    description: 'Соревнования инспекторов военной автомобильной инспекции'
+                },
+                {
+                    id: 30,
+                    title: "Соколиная охота",
+                    image: "26-bla.png",
+                    description: 'Соревнование экипажей БПЛА'
+                },
+                {
+                    id: 31,
+                    title: "Авиадартс",
+                    image: "4-darts.png",
+                    description: 'Соревнования летных экипажей'
+                },
+                {
+                    id: 32,
+                    title: "Полевая кухня",
+                    image: "22-kuhnya.png",
+                    description: 'Соревнования специалистов продовольственной службы'
+                }
+            ]
+        },
+        'left-bottom': {
+            'top': 1336,
+            'left': 84,
+            'data': [
+                {
+                    id: 33,
+                    title: "Тактический стрелок",
+                    image: "",
+                    description: 'Тест'
+                },
+                {
+                    id: 34,
+                    title: "Полярная звезда",
+                    image: "30-zvezda.png",
+                    description: 'Соревнования подразделений специального назначений'
+                },
+                {
+                    id: 35,
+                    title: "Деснатный взвод",
+                    image: "5-desvzvod.png",
+                    description: 'Тест'
+                },
+                {
+                    id: 36,
+                    title: "Глубина",
+                    image: "8-glubina.png",
+                    description: 'Соревнования водолазов'
+                },
+                {
+                    id: 37,
+                    title: "Морской десант",
+                    image: "6-mordes.png",
+                    description: 'Соревнования подразделений морской пехоты'
+                },
+                {
+                    id: 38,
+                    title: "Кубок моря",
+                    image: "7-more.png",
+                    description: 'Соревнования экипажей надводных кораблей'
+                },
+            ]
+        },
+        'right-top': {
+            'top': 410,
+            'right': 12,
+            'data': [
+                {
+                    id: 19,
+                    title: "Федерация \"Танковый биатлон\"",
+                    description: 'Тест'
+                },
+                {
+                    id: 18,
+                    title: "Судейский корпус АрМИ",
+                    description: 'Тест'
+                },
+                {
+                    id: 17,
+                    title: "Верный друг",
+                    image: "20-drug.png",
+                    description: 'Совернования специалистов служебного собаководства'
+                },
+                {
+                    id: 16,
+                    title: "Воин мира",
+                    image: "25-voin.png",
+                    description: 'Соревнования военно-профессионального мастерства военнослужащих'
+                },
+                {
+                    id: 15,
+                    title: "Мастер оружейник",
+                    image: "10-oruzheinik.png",
+                    description: 'Соревнования ремонтных взводов вооружения'
+                },
+                {
+                    id: 14,
+                    title: "Военная-медицинская эстафета",
+                    image: "21-med.png",
+                    description: 'Соревнования медицинского персонала'
+                },
+                {
+                    id: 13,
+                    title: "Инженерная формула",
+                    image: "16-formula.png",
+                    description: 'Соревнования экипажей машин инженерного вооружения'
+                },
+                {
+                    id: 12,
+                    title: "Безопасный маршрут",
+                    image: "15-marshrut.png",
+                    description: 'Соревнования инженерно-саперных подразделений'
+                },
+                {
+                    id: 11,
+                    title: "Открытая вода",
+                    image: "14-voda.png",
+                    description: 'Соревнования понтонно-переправочных подразделений'
+                },
+                {
+                    id: 10,
+                    title: "Аварийный район",
+                    image: "28-raion.png",
+                    description: 'Состязания аварийно-спасательных формирований'
+                },
+
+                {
+                    id: 9,
+                    title: "Эльбрусское кольцо",
+                    image: "19-elbrus.png",
+                    description: 'Соревнования горных подразделений'
+                },
+                {
+                    id: 8,
+                    title: "Саянский марш",
+                    image: "31-sayan-march.png",
+                    description: 'Соревнования военнослужащих горных подразделений в зимних условиях'
+                },
+                {
+                    id: 7,
+                    title: "Отличники войсковой разметки",
+                    description: 'Тест'
+                }
+            ]
+        },
+        'right-bottom': {
+            'top': 1320,
+            'right': 28,
+            'data': [
+                {
+                    id: 6,
+                    title: "Чистое небо",
+                    image: "11-nebo.png",
+                    description: 'Соревнования подразделений ПВО'
+                },
+                {
+                    id: 5,
+                    title: "Мастер артиллерийского огня",
+                    image: "9-artogon.png",
+                    description: 'Соревнования минометных расчетов'
+                },
+                {
+                    id: 4,
+                    title: "Конный марафон",
+                    image: "29-koni.png",
+                    description: 'Соревнования конных подразделений'
+                },
+                {
+                    id: 3,
+                    title: "Снайперский рубеж",
+                    image: "3-rubezh.png",
+                    description: 'Соревнования снайперов'
+                },
+                {
+                    id: 2,
+                    title: "Суворовский натиск",
+                    image: "2-natisk.png",
+                    description: 'Соревнования экипажей боевых машин пехоты'
+                },
+                {
+                    id: 1,
+                    title: "Танковый биатлон",
+                    image: "1-biatlon.png",
+                    description: 'Соревнования танковых экипажей'
+                }
+            ]
+        }
+    };
+
+let idleTimer = null;
+let idleState = false;
+let idleWait = 40000;
+
+const idleStateFunc = function(){
+
+    let idleHandler = function(){
+        clearTimeout(idleTimer); // отменяем прежний временной отрезок
+        if(idleState === true){
+
+        }
+        idleState = false;
+        idleTimer = setTimeout(function(){
+            let idleScreen = document.getElementsByClassName('idle-screen')[0];
+            idleScreen.classList.remove("idle-screen_hide");
+            idleScreen.classList.add("idle-screen_show");
+
+            idleState = true;
+        }, idleWait);
+    }
+
+    document.addEventListener('mousemove', idleHandler);
+    document.addEventListener('scroll', idleHandler);
+    document.addEventListener('keydown', idleHandler);
+    document.addEventListener('touchstart', idleHandler);
+
+    let event = new Event('mousemove');
+    document.dispatchEvent(event);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const isVisible = "is-visible";
+    const modalClose = document.getElementsByClassName('close-modal');
+    const modalHeader = document.getElementsByClassName('modal-header');
+    const modalText = document.getElementsByClassName('modal-content__text');
+    const modalImage = document.getElementsByClassName('modal-content__image');
+
+    modalClose[0].addEventListener("click", () => {
+      document.getElementById('modal-info').classList.remove(isVisible);
+    });
+
+    // document.addEventListener("click", e => {
+    //     console.log('e.target', e.target);
+    //     const modal = document.querySelector(".modal");
+    //
+    //     console.log('modal', modal.classList.contains("is-visible"));
+    //
+    //
+    //     if (e.target === document.querySelector(".modal.is-visible")) {
+    //         // document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    //     }
+    // });
+
+    Object.keys(stands).forEach( position => {
+       let stand = stands[position];
+       let legendBox = document.getElementsByClassName(`legend-${position}`)[0];
+           legendBox.style.top = `${stand.top}px`;
+
+       if(position === 'left-top' || position === 'left-bottom'){
+           legendBox.style.left = `${stand.left}px`;
+       }else{
+           legendBox.style.right = `${stand.right}px`;
+       }
+
+       stand.data.forEach( stand => {
+           console.log(stand)
+            let legendItem = document.createElement("div");
+                legendItem.className = `legend-item ${position === 'left-top' || position === 'left-bottom' ? '' : 'legend-item_reverse'}`;
+                legendItem.id = stand.id;
+                legendItem.innerHTML = `
+                   
+                    <span class="legend-item__title">${stand.title}</span>
+                    <div class="cube legend-item__highlight">
+                          <div class="side top"></div>
+                          <div class="side left"></div>
+                          <div class="side front"></div>
+                    </div>
+                     ${stand.image ? `<img class="legend-item__icon" src='assets/images/${stand.image}' alt="">` : '<div class="no-image"></div>'}
+                    `;
+
+                legendItem.addEventListener("click", function(e) {
+                    modalHeader[0].textContent = `${stand.title}`;
+                    modalText[0].textContent = `${stand.description}`;
+                    modalImage[0].src = `assets/images-modal/${stand.id}.jpg`
+                   document.getElementById('modal-info').classList.add(isVisible);
+               }, false);
+           legendBox.appendChild(legendItem);
+       })
+   })
+
+    document.getElementsByClassName('army-cup')[0].addEventListener("click", function(e) {
+        modalHeader[0].textContent = `Кубок АрМИ`;
+        modalText[0].textContent = `Конкурс организуется и проводится в целях:
+                                    - укрепление дружбы, товарищества и взаимопонимания
+                                    между народами и вооруженными силами стран – участниц АрМИ2020;
+                                    - популяризации военно-патриотической песни;
+                                    - выявление и развитие творческих способностей участников
+                                    конкурса;
+                                    - предоставлении возможности реализации своего творческого
+                                    потенциала коллективом и исполнителем стран-участниц АрМИ2020. `;
+
+        modalImage[0].src = 'assets/images-modal/cup.jpg'
+
+        document.getElementById('modal-info').classList.add(isVisible);
+    }, false);
+
+    idleStateFunc();
+
+    document.getElementsByClassName('idle-screen__start')[0].addEventListener('click', function() {
+        let idleScreen = document.getElementsByClassName('idle-screen')[0];
+        idleScreen.classList.remove("idle-screen_show");
+        idleScreen.classList.add("idle-screen_hide");
+    })
+});
